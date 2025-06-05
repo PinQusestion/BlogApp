eebd6d54d4f436ecc68d132f823d7c562d9c623d
PinQusestion <mananbansal238@gmail.com>
2025-06-06 00:37:25 +0530

    changed the UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -42,11 +42,11 @@ function Header() {
   ];
 
   return (
-    <header className="top-0 fixed left-0 w-full z-50 py-4 shadow bg-[#1d293b] text-white">
+    <header className="top-0 fixed left-0 w-full z-50 py-4 shadow bg-[#121928] text-white">
       <Container>
         <nav className="flex text-center">
           {authStatus ? (
-            <div className="text-2xl text-[#706df9] mt-1 font-bold">BlogSpace</div>
+            <div className="text-3xl text-transparent bg-clip-text bg-gradient-to-r from-[#66a3f8] to-[#B283F2] font-bold">BlogSpace</div>
           ) : (
             <div className="text-2xl text-[#706df9]">
               BlogSpace
@@ -59,7 +59,7 @@ function Header() {
                 <li key={item.name}>
                     <button
                       onClick={() => navigate(item.slug)}
-                      className="inline-flex items-center px-6 py-1.5 duration-200 hover:text-cyan-400 rounded-full gap-1.5"
+                      className="inline-flex items-center px-6 py-1.5 duration-200 hover:text-cyan-400 hover:cursor-pointer rounded-full gap-1.5"
                     >
                       <Icon icon={item.icon} className="group-hover:stroke-cyan-400 active:stroke-cyan-400"/>
                       {item.name}
